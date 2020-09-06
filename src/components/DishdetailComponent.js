8e1c5532124b5178d8910efeb60fbaed21913fd6
Abdo KD <abdokardidi44@gmail.com>
2020-09-06 15:59:53 +0100

    Submit Comment Redux Form Updated

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,12 +1,110 @@
 import React from 'react';
 import {
     Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem
+    CardTitle, Breadcrumb, BreadcrumbItem,
+    Button, Row, Col, Label, Modal, ModalHeader, ModalBody
 } from 'reactstrap';
+
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
-import CommentForm from './CommentForm';
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+
+class CommentForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isModalOpen: false
+        };
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handelSubmitComment = this.handelSubmitComment.bind(this);
+    }
 
 
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+    handelSubmitComment(values) {
+        this.toggleModal();
+        alert(JSON.stringify(values));
+
+
+    }
+    render() {
+        return (
+            <div>
+
+
+                <Button outline onClick={this.toggleModal}><span className="fa fa-pencil fa-lg"></span> Submit Comment</Button>
+
+
+
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={(values) => this.handelSubmitComment(values)}>
+                            <Row>
+                                <Label htmlFor="rating" className="labels" md={12}>Rating</Label>
+                                <Col md={12}>
+                                    <Control.select model=".rating" name="rating" id="rating"
+                                        className="form-control">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </Control.select>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="name" className="labels" md={12}>Your Name</Label>
+                                <Col md={12}>
+                                    <Control.text model=".name" id="name" name="name"
+                                        placeholder="Your Name"
+                                        className="form-control"
+                                        validators={{
+                                            minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".name"
+                                        show="touched"
+                                        messages={{
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="comment" className="labels" md={12}>Comment</Label>
+                                <Col md={12}>
+                                    <Control.textarea model=".comment" id="comment" name="comment"
+                                        rows="6"
+                                        className="form-control" />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={12}>
+                                    <Button type="submit" color="primary">
+                                        Submit
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </div>
+        );
+
+
+    }
+}
 
 function RenderDish({ dish }) {
     if (dish != null) {
@@ -66,7 +164,7 @@ function RenderComments({ comments }) {
                         <p>{comment.comment}</p>
                         <p>--{comment.author}{', '}{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
                     </div>
-                    
+
                 </div>
 
             );
@@ -74,7 +172,7 @@ function RenderComments({ comments }) {
         return (
             <div>
                 {Dishcomments}
-                <CommentForm/>
+                <CommentForm />
             </div>
         )
     }
