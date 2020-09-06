d492ea56803fcb72a5ca3617f454b6c164c858a3
Abdo KD <abdokardidi44@gmail.com>
2020-09-06 22:38:59 +0100

    Combining Reducers

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -52,11 +52,11 @@ class CommentForm extends React.Component {
                                 <Col md={12}>
                                     <Control.select model=".rating" name="rating" id="rating"
                                         className="form-control">
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
                                     </Control.select>
                                 </Col>
                             </Row>
